b0b551a2f2784fde886a37161ec6c923b80f5f53
kochilLu <72820763+kochilLu@users.noreply.github.com>
2025-02-14 12:53:21 +0000

    Created a package "DTOs" and populated it with 2 implemented java classes "Expense" and "Income" which will be used to create Expense type objects and Income type objects.

---
+++ src/main/java/BusinessObjects/Main.java
@@ -0,0 +1,9 @@
+package BusinessObjects;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //...use name scape of sample 4!
+
+    }
+}
---
+++ src/main/java/DTOs/Expense.java
@@ -0,0 +1,49 @@
+package DTOs;
+
+import java.time.LocalDate;
+
+public class Expense {
+
+    int id;
+    String title;
+    String category;
+    double amountSpent;
+    LocalDate date;
+
+    //full constructor
+    public Expense(int id, String title, String category, double amountSpent, LocalDate date) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.amountSpent = amountSpent;
+        this.date = date;
+    }
+
+    //partial constructor
+    public Expense(String title, String category, double amountSpent, LocalDate date) {
+        this.id = 0;
+        this.title = title;
+        this.category = category;
+        this.amountSpent = amountSpent;
+        this.date = date;
+    }
+
+    //getters
+    public int getId(){return id;}
+    public String getTittle(){return title;}
+    public String getCategory(){return category;}
+    public double getAmountSpent(){return amountSpent;}
+    public LocalDate getDate(){return date;}
+
+    //setters
+    public void setId(int id){this.id = id;}
+    public void setTittle(String title){this.title = title;}
+    public void setCategory(String category){this.category = category;}
+    public void setAmountSpent(double amountSpent){this.amountSpent = amountSpent;}
+    public void setDate(LocalDate date){this.date = date;}
+
+    @Override
+    public String toString() {
+        return "Expense{id: " + id + ", title: " + title + ", category: " + category + ", amountSpent: " + amountSpent + ", date: " + date + "}";
+    }
+}
